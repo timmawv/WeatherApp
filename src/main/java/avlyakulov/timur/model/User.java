8e69bb172baa8d35209e057b9cde5cf3b9eb9af1
Timur <91984444+timcol1@users.noreply.github.com>
2024-03-06 14:18:07 +0200

    create user model

---
+++ src/main/java/avlyakulov/timur/model/User.java
@@ -0,0 +1,30 @@
+package avlyakulov.timur.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String login;
+
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
