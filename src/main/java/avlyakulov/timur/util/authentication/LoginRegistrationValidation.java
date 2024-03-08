f8e557a47159b6aef46c40460ee21bc061bd167f
Timur <91984444+timcol1@users.noreply.github.com>
2024-03-08 13:52:25 +0200

    add more relative error field checker

--- src/main/java/avlyakulov/timur/util/authentication/LoginRegistrationValidation.java
+++ src/main/java/avlyakulov/timur/util/authentication/LoginRegistrationValidation.java
@@ -7,23 +7,16 @@ import org.thymeleaf.context.Context;
 public class LoginRegistrationValidation {
     private static final String passwordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$";
 
+    private static final String nameFieldError = "error_field";
+
     public static boolean isFieldEmpty(Context context, String... parameters) {
         for (String param : parameters) {
             if (param == null || param.isBlank()) {
-                context.setVariable("empty_field_error", "One or more fields are empty. Please avoid empty fields");
+                setErrorToContext(context, "One or more fields are empty. Please avoid empty fields");
                 return true;
             }
         }
         return false;
-    }
-
-    private static boolean isPasswordsTheSame(String password, String confirmPassword, Context context) {
-        if (password.equals(confirmPassword)) {
-            return true;
-        } else {
-            context.setVariable("confirm_password_error", "The passwords aren't same. Please enter the same passwords");
-            return false;
-        }
     }
 
     public static boolean isPasswordTheSameAndStrong(String password, String confirmPassword, Context context) {
@@ -35,20 +28,32 @@ public class LoginRegistrationValidation {
                     if (strength.getScore() >= 3) {
                         return true;
                     } else {
-                        context.setVariable("password_strength_error", "Your password is to easy. Here is some suggestions to help you");
-                        context.setVariable("suggestions", strength.getFeedback().getSuggestions());
+                        setErrorToContext(context, "Your password is to easy. Here is some suggestions to help you <br>" + strength.getFeedback().getSuggestions());
                         return false;
                     }
                 } else {
-                    context.setVariable("password_matcher_error", "Your password must contain one capital letter one small letter and one number");
+                    setErrorToContext(context, "Your password must contain one capital letter one small letter and one number");
                     return false;
                 }
             } else {
-                context.setVariable("password_length_error", "The length of password has to be from 6 to 16");
+                setErrorToContext(context, "The length of password has to be from 6 to 16");
                 return false;
             }
         } else {
             return false;
         }
+    }
+
+    private static boolean isPasswordsTheSame(String password, String confirmPassword, Context context) {
+        if (password.equals(confirmPassword)) {
+            return true;
+        } else {
+            setErrorToContext(context, "The passwords aren't same. Please enter the same passwords");
+            return false;
+        }
+    }
+
+    private static void setErrorToContext(Context context, String errorMessage) {
+        context.setVariable(nameFieldError, errorMessage);
     }
 }
