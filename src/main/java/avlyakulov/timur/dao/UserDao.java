00975924290e5018062e253cc979b612f80c238d
Timur <91984444+timcol1@users.noreply.github.com>
2024-03-11 09:28:17 +0200

    created UserDao

---
+++ src/main/java/avlyakulov/timur/dao/UserDao.java
@@ -0,0 +1,14 @@
+package avlyakulov.timur.dao;
+
+import avlyakulov.timur.model.User;
+
+public class UserDao {
+
+    public void create(User user) {
+
+    }
+
+    public User getById(int userId) {
+        return new User();
+    }
+}
