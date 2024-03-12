ef25f27776940919bb17864a678931106e8858e6
Timur <91984444+timcol1@users.noreply.github.com>
2024-03-12 12:59:44 +0200

    create sessionDao and sessionService to deal with session in db

---
+++ src/main/java/avlyakulov/timur/dao/SessionDao.java
@@ -0,0 +1,32 @@
+package avlyakulov.timur.dao;
+
+import avlyakulov.timur.custom_exception.ModelNotFoundException;
+import avlyakulov.timur.model.Session;
+import avlyakulov.timur.util.HibernateSingletonUtil;
+import jakarta.persistence.NoResultException;
+import org.hibernate.SessionFactory;
+
+import java.util.UUID;
+
+public class SessionDao {
+    private final SessionFactory sessionFactory = HibernateSingletonUtil.getSessionFactory();
+
+
+    public void create(Session session) {
+        try (org.hibernate.Session hibernateSession = sessionFactory.openSession()) {
+            hibernateSession.beginTransaction();//открываем транзакцию
+
+            hibernateSession.persist(session);
+
+            hibernateSession.getTransaction().commit();//закрываем транзакцию
+        }
+    }
+
+    public Session getById(UUID sessionId) {
+        try (org.hibernate.Session hibernateSession = sessionFactory.openSession()) {
+            return hibernateSession.get(Session.class, sessionId);
+        } catch (NoResultException e) {
+            throw new ModelNotFoundException("Session with such id doesn't exist");
+        }
+    }
+}
---
+++ src/main/java/avlyakulov/timur/service/SessionService.java
@@ -0,0 +1,26 @@
+package avlyakulov.timur.service;
+
+import avlyakulov.timur.dao.SessionDao;
+import avlyakulov.timur.model.Session;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class SessionService {
+
+    private final SessionDao sessionDao = new SessionDao();
+
+    public void createSession(Session session) {
+        sessionDao.create(session);
+    }
+
+    public Session getSessionById(UUID sessionId) {
+        return sessionDao.getById(sessionId);
+    }
+
+    public boolean isUserSessionExpired(UUID sessionId) {
+        Session session = sessionDao.getById(sessionId);
+        LocalDateTime now = LocalDateTime.now();
+        return now.isBefore(session.getExpiresAt());
+    }
+}
