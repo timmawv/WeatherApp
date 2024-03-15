02d59c8d7533d7471d7232e0ada6885184d31859
Timur <91984444+timcol1@users.noreply.github.com>
2024-03-15 09:34:20 +0200

    create userDto to send login

---
+++ src/main/java/avlyakulov/timur/dto/UserDto.java
@@ -0,0 +1,17 @@
+package avlyakulov.timur.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private int userId;
+
+    private String login;
+}
