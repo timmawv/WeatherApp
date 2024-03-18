67875e538e785eec06f4accdae89adbe49aa5654
Timur <91984444+timcol1@users.noreply.github.com>
2024-03-18 13:16:25 +0200

    add sunrise and sunset animated icon

--- src/main/java/avlyakulov/timur/util/api/GetUrlIconOfWeatherByIcon.java
+++ src/main/java/avlyakulov/timur/util/api/GetUrlIconOfWeatherByIcon.java
@@ -1,8 +1,25 @@
 package avlyakulov.timur.util.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GetUrlIconOfWeatherByIcon {
+    private Map<String, String> iconUrls = new HashMap<>(
+            Map.of(
+                    "01d", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/day.svg?raw=true",
+                    "01n", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/night.svg?raw=true",
 
+                    "02d", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/cloudy-day-1.svg?raw=true",
+                    "02n", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/cloudy-night-1.svg?raw=true",
 
+                    "03d", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/cloudy.svg?raw=true",
+                    "03n", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/cloudy.svg?raw=true",
+
+                    "04d", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/cloudy.svg?raw=true",
+                    "04n", "https://github.com/timmawv/WeatherApp/blob/main/img/animated/cloudy.svg?raw=true",
+            )
+
+    );
     public static String getUrlWeatherIcon(String iconCode) {
         final String urlIconWeather = "https://openweathermap.org/img/wn/";
         String icon = iconCode.concat("@2x.png");
