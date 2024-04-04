66368f2416f45b7dfdd0e7daf27104be4d91a254
Timur <91984444+timcol1@users.noreply.github.com>
2024-04-04 08:56:32 +0300

    fix bug with locations

--- src/main/java/avlyakulov/timur/dto/UserDto.java
+++ src/main/java/avlyakulov/timur/dto/UserDto.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserDto {
 
-    private int userId;
+    private Integer userId;
 
     private String login;
 }
--- src/main/java/avlyakulov/timur/mapper/UserMapper.java
+++ src/main/java/avlyakulov/timur/mapper/UserMapper.java
@@ -3,11 +3,13 @@ package avlyakulov.timur.mapper;
 import avlyakulov.timur.dto.UserDto;
 import avlyakulov.timur.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(source = "id", target = "userId")
     UserDto mapUserToUserDto(User user);
 }
