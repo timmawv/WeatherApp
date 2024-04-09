551895c80fa2c88a6dec6d80c4cbb5531a243784
Timur <91984444+timcol1@users.noreply.github.com>
2024-04-09 09:00:11 +0300

    add healthcheck for Docker

---
+++ src/main/java/avlyakulov/timur/servlet/TestController.java
@@ -0,0 +1,25 @@
+package avlyakulov.timur.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/test-back")
+public class TestController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "ok");
+        resp.getWriter().println(objectMapper.writeValueAsString(response));
+        resp.setContentType("application/json");
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+}
