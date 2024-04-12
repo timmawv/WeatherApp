620698c1891132fc1239c4fabed11f986c50d67d
Timur <91984444+timcol1@users.noreply.github.com>
2024-04-12 08:58:06 +0300

    add feature with full and short view of weather

--- src/main/resources/static/js/removeFavorite.js
+++ src/main/resources/static/js/removeFavorite.js
@@ -1,7 +1,11 @@
 function removeFavorite() {
+
     const buttons = document.querySelectorAll('.button');
+
     buttons.forEach(function (button) {
+
         const form = button.closest('form');
+
         button.addEventListener("click", function () {
             button.classList.toggle('active');
             form.submit();
--- src/main/resources/static/js/viewButton.js
+++ src/main/resources/static/js/viewButton.js
@@ -1,21 +1,36 @@
 function viewButton() {
 
-    var buttonOne = document.querySelector('.btn.btn-light.one');
-    var buttonTwo = document.querySelector('.btn.btn-light.two');
+    const buttonOne = document.querySelector('.btn.btn-outline-primary.one');
+    const buttonTwo = document.querySelector('.btn.btn-outline-primary.two');
+
+    const forecastBlocksFull = document.querySelectorAll(".forecast.full");
+    const forecastBlocksShort = document.querySelectorAll(".forecast.short");
 
     buttonOne.addEventListener('click', function () {
-        var isActive = buttonOne.classList.contains('active');
+        const isActive = buttonOne.classList.contains('active');
         if (!isActive) {
             buttonOne.classList.toggle('active');
             buttonTwo.classList.remove('active');
+            forecastBlocksFull.forEach(function (block) {
+                block.style.display = "inline-block";
+            });
+            forecastBlocksShort.forEach(function (block) {
+                block.style.display = "none";
+            });
         }
     });
 
     buttonTwo.addEventListener('click', function () {
-        var isActive = buttonTwo.classList.contains('active');
+        const isActive = buttonTwo.classList.contains('active');
         if (!isActive) {
             buttonOne.classList.remove('active');
             buttonTwo.classList.toggle('active');
+            forecastBlocksFull.forEach(function (block) {
+                block.style.display = "none";
+            });
+            forecastBlocksShort.forEach(function (block) {
+                block.style.display = "inline-block";
+            });
         }
     });
 }
